9d892ac56e89926472e33b43a1f3961c5adad809
T. E. Pickering <te.pickering@gmail.com>
2017-10-31 22:55:11 -0400

    switch logger to log everything

--- wfssrv/wfssrv.py
+++ wfssrv/wfssrv.py
@@ -37,7 +37,7 @@ from mmtwfs.wfs import WFSFactory
 from mmtwfs.zernike import ZernikeVector
 from mmtwfs.telescope import MMT
 
-log = logging.getLogger('tornado.application')
+log = logging.getLogger('')
 log.setLevel(logging.INFO)
 
 
