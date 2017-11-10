74bb24cf1d9558b783c28d80883b898a30001ce1
T. E. Pickering <te.pickering@gmail.com>
2017-11-09 23:47:56 -0700

    modify sao wfs server to flip binospec image up/down

--- scripts/wfs_header.py
+++ scripts/wfs_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import numpy as np
 from astropy.io import fits
 from camsrv.header import update_header
 
@@ -9,14 +10,13 @@ if __name__ == "__main__":
     filename = sys.argv[1]
     hdu = fits.open(filename)
     newhdu = update_header(hdu)
-    newhdu.writeto(filename, overwrite=True, output_verify="silentfix")
 
     # now check for identifying header information
     if isinstance(newhdu, fits.hdu.image.PrimaryHDU):
         header = newhdu.header
     elif isinstance(newhdu, list):
         header = None
-        h = newhdu.pop()
+        h = newhdu[1]
         header = h.header
         if header is None:
             raise ValueError("No PrimaryHDU found in HDU list.")
@@ -34,6 +34,7 @@ if __name__ == "__main__":
     if 'ORIGIN' in header:
         if 'Binospec' in header['ORIGIN']:
             inst = "binospec"
+            newhdu[1].data = np.flipud(newhdu[1].data)
 
     # check for F/9
     if 'OBSERVER' in header:
@@ -44,5 +45,7 @@ if __name__ == "__main__":
     if inst is None and 'SEC' in header:  # mmirs has SEC in header as well which is why we check if inst isn't set
         if 'F5' in header['SEC']:
             inst = "f5"
+
+    newhdu.writeto(filename, overwrite=True, output_verify="silentfix")
 
     print(inst)
