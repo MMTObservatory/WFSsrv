7dc43cff6199f4dc975f053a5d553226b877217d
T. E. Pickering <te.pickering@gmail.com>
2020-06-08 18:07:23 -0700

    add back test for cwfs server; tweak actions config to run tests on every push, but docker only when pushed to master or PR merged there

--- wfssrv/tests/test_app.py
+++ wfssrv/tests/test_app.py
@@ -2,8 +2,14 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 from ..wfssrv import WFSsrv as wfs
+from ..cwfssrv import WFSsrv as cwfs
 
 
 def test_wfs():
     app = wfs()
+    assert(app is not None)
+
+
+def test_cwfs():
+    app = cwfs()
     assert(app is not None)
